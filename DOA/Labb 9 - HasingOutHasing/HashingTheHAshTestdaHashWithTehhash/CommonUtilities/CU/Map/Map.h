65e1435f3049aae7c003884dfbcb84107a0b6490
Simon Skogsrydh <simon.skogsrydh.sp15@thegameassembly.com>
2016-05-09 17:35:05 +0200

    finished hash map.

--- DOA/Labb 9 - HasingOutHasing/HashingTheHAshTestdaHashWithTehhash/CommonUtilities/CU/Map/Map.h
+++ DOA/Labb 9 - HasingOutHasing/HashingTheHAshTestdaHashWithTehhash/CommonUtilities/CU/Map/Map.h
@@ -3,14 +3,35 @@
 
 namespace CommonUtilities
 {
+	template <typename Key, typename Value, typename CountType = unsigned short>
+	struct NodeData
+	{
+		NodeData()
+		{}
+		NodeData(const Key & aKey)
+			: myKey(aKey)
+		{
+		}
+
+		Key myKey;
+		Value myValue;
+	};
 
 	template <typename Key, typename Value, typename CountType = unsigned short>
 	class Map
 	{
 	public:
-		Map()
+		
+
+		Map(const CountType & aSize = 128)
 		{
-			
+			using std::placeholders::_1;
+			myData.Init(aSize);
+			myData.Resize(aSize);
+			for (CountType iData = 0; iData < myData.Size(); ++iData)
+			{
+				myData[iData].Init(3);
+			}
 		}
 		~Map()
 		{}
@@ -22,25 +43,79 @@ namespace CommonUtilities
 		void Delete(const Key & aKey); // assert på key
 
 	private:
-		GrowingArray<Value, CountType> mystuff;
+		CountType GetIndex(const Key & aKey);
+		NodeData<Key, Value, CountType>& AddToPosition(const Key & aKey);
+
+		GrowingArray<GrowingArray<NodeData<Key, Value, CountType>, CountType>, CountType> myData;
 	};
 
 	template <typename Key, typename Value, typename CountType /*= unsigned short*/>
-	Value& CommonUtilities::Map<Key, Value, CountType>::operator[](const Key & aKey)
+	NodeData<Key, Value, CountType>& CommonUtilities::Map<Key, Value, CountType>::AddToPosition(const Key & aKey)
 	{
+		myData[GetIndex(aKey)].Add(NodeData<Key, Value, CountType>(aKey));
+		return myData[GetIndex(aKey)].GetLast();
+	}
+	
 
+	template <typename Key, typename Value, typename CountType /*= unsigned short*/>
+	CountType CommonUtilities::Map<Key, Value, CountType>::GetIndex(const Key & aKey)
+	{
+		return std::hash<Key>()(aKey) % myData.Size();
 	}
 
 	template <typename Key, typename Value, typename CountType /*= unsigned short*/>
-	bool CommonUtilities::Map<Key, Value, CountType>::KeyExists(const Key & aKey)
+	inline Value& CommonUtilities::Map<Key, Value, CountType>::operator[](const Key & aKey)
 	{
+		if (KeyExists(aKey) == true)
+		{
+			for (CountType iData = 0; iData < myData[GetIndex(aKey)].Size(); ++iData)
+			{
+				if (myData[GetIndex(aKey)][iData].myKey == aKey)
+				{
+					return myData[GetIndex(aKey)][iData].myValue;
+				}
+			}
+		}
 
+		return AddToPosition(aKey).myValue;
 	}
 
 	template <typename Key, typename Value, typename CountType /*= unsigned short*/>
-	void CommonUtilities::Map<Key, Value, CountType>::Delete(const Key & aKey)
+	inline bool CommonUtilities::Map<Key, Value, CountType>::KeyExists(const Key & aKey)
 	{
+		const CountType Index = GetIndex(aKey);
+		
+		if (myData[Index].Size() > 0)
+		{
+			for (CountType iData = 0; iData < myData[Index].Size(); ++iData)
+			{
+				if (myData[Index][iData].myKey == aKey)
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+		else
+		{
+			return false;
+		}
+	}
 
+	template <typename Key, typename Value, typename CountType /*= unsigned short*/>
+	inline void CommonUtilities::Map<Key, Value, CountType>::Delete(const Key & aKey)
+	{
+		DL_ASSERT(KeyExists(aKey) == true, "Key does not Exist");
+
+		const CountType Index = GetIndex(aKey);
+
+		for (CountType iData = 0; iData < myData[Index].Size(); ++iData)
+		{
+			if (myData[Index][iData].myKey == aKey)
+			{
+				myData[Index].RemoveCyclicAtIndex(iData);
+			}
+		}
 	}
 	
 
--- DOA/Labb 9 - HasingOutHasing/HashingTheHAshTestdaHashWithTehhash/HashingTheHAshTestdaHashWithTehhash/unittest1.cpp
+++ DOA/Labb 9 - HasingOutHasing/HashingTheHAshTestdaHashWithTehhash/HashingTheHAshTestdaHashWithTehhash/unittest1.cpp
@@ -3,16 +3,108 @@
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
+#include <CU/Map/Map.h>
+#include <CU/NameSpaceAliases.h>
+
 namespace HashingTheHAshTestdaHashWithTehhash
 {		
 	TEST_CLASS(UnitTest1)
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(CreateTheMap)
 		{
-			// TODO: Your test code here
+			CU::Map<std::string, int> test;
 		}
 
+		TEST_METHOD(KeyExists)
+		{
+			CU::Map<std::string, int> testMap;
+
+			Assert::IsFalse(testMap.KeyExists("derp"));
+		}
+
+		TEST_METHOD(SetValue)
+		{
+			CU::Map<std::string, int> testMap;
+
+			testMap["derp"] = 3;
+
+			Assert::IsTrue(testMap.KeyExists("derp"));
+			Assert::IsFalse(testMap.KeyExists("herp"));
+		}
+
+		TEST_METHOD(AccessValue)
+		{
+			CU::Map<std::string, int> testMap;
+
+			testMap["derp"] = 3;
+
+			Assert::IsTrue(testMap["derp"] == 3);
+		}
+
+		TEST_METHOD(AccessManyValues)
+		{
+			CU::Map<std::string, int> testMap;
+
+			testMap["derp"] = 3;
+			testMap["herp"] = 1;
+			testMap["merp"] = 4;
+			testMap["serp"] = 5;
+
+			Assert::IsTrue(testMap["derp"] == 3);
+			Assert::IsTrue(testMap["herp"] == 1);
+			Assert::IsTrue(testMap["merp"] == 4);
+			Assert::IsTrue(testMap["serp"] == 5);
+			Assert::IsFalse(testMap.KeyExists("klerk"));
+		}
+
+		TEST_METHOD(DeleteValue)
+		{
+			CU::Map<std::string, int> testMap;
+
+			testMap["derp"] = 3;
+			testMap["herp"] = 1;
+
+			Assert::IsTrue(testMap["derp"] == 3);
+			Assert::IsTrue(testMap["herp"] == 1);
+
+			testMap.Delete("herp");
+
+			Assert::IsTrue(testMap.KeyExists("derp"));
+			Assert::IsTrue(testMap["derp"] == 3);
+			Assert::IsFalse(testMap.KeyExists("herp"));
+		}
+
+		TEST_METHOD(TestWithStrings)
+		{
+			CU::Map<std::string, std::string> testMap;
+
+			Assert::IsFalse(testMap.KeyExists("lerp"));
+
+			testMap["lerp"] = "YlvaBehöverSova";
+
+			Assert::IsTrue(testMap.KeyExists("lerp"));
+
+			Assert::IsTrue(testMap["lerp"] == "YlvaBehöverSova");
+		}
+
+		TEST_METHOD(TestWithPointers)
+		{
+			CU::Map<std::string, float*> testMap;
+
+			float * testPointer1 = new float(3.f);
+			float * testPointer2 = new float(4.3f);
+
+			Assert::IsFalse(testMap.KeyExists("lerp"));
+
+			testMap["lerp"] = testPointer1;
+			testMap["ylf"] = testPointer2;
+
+			Assert::IsTrue(testMap.KeyExists("lerp"));
+
+			Assert::IsTrue(testMap.KeyExists("ylf"));
+			Assert::IsTrue(testMap["lerp"] == testPointer1);
+		}
 	};
 }
