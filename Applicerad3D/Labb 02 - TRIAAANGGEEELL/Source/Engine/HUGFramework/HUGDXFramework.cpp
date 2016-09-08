ec9aa4f6cbec1453932d8a4d4d222962c813e9f8
Ylf <ylva.werner.sp15@thegameassembly.com>
2016-09-08 14:30:44 +0200

    Working on the setupsies

--- Applicerad3D/Labb 02 - TRIAAANGGEEELL/Source/Engine/HUGFramework/HUGDXFramework.cpp
+++ Applicerad3D/Labb 02 - TRIAAANGGEEELL/Source/Engine/HUGFramework/HUGDXFramework.cpp
@@ -13,6 +13,15 @@ CHUGDXFramework::CHUGDXFramework()
 
 CHUGDXFramework::~CHUGDXFramework()
 {
+	mySwapChain->Release();
+	myDevice->Release();
+	myDeviceContext->Release();
+	myRenderTargetView->Release();
+
+	mySwapChain = nullptr;
+	myDevice = nullptr;
+	myDeviceContext = nullptr;
+	myRenderTargetView = nullptr;
 }
 
 void CHUGDXFramework::Init(void* aHWND, const CU::Vector2ui & aScreenWidthHeight)
@@ -104,12 +113,112 @@ void CHUGDXFramework::Init(void* aHWND, const CU::Vector2ui & aScreenWidthHeight
 	tempBackBufferPtr->Release();
 	tempBackBufferPtr = nullptr;
 
-	
-	//TODO^^ creata rendertargetview med backbuffer
+	// Initialize the description of the depth buffer.
+	D3D11_TEXTURE2D_DESC tempDepthBufferDescription;
+	ZeroMemory(&tempDepthBufferDescription, sizeof(tempDepthBufferDescription));
 
-	//TODO^^ releasa backbuffer
+	// Set up the description of the depth buffer.
+	tempDepthBufferDescription.Width = aScreenWidthHeight.x;
+	tempDepthBufferDescription.Height = aScreenWidthHeight.y;
+	tempDepthBufferDescription.MipLevels = 1;
+	tempDepthBufferDescription.ArraySize = 1;
+	tempDepthBufferDescription.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+	tempDepthBufferDescription.SampleDesc.Count = 1;
+	tempDepthBufferDescription.SampleDesc.Quality = 0;
+	tempDepthBufferDescription.Usage = D3D11_USAGE_DEFAULT;
+	tempDepthBufferDescription.BindFlags = D3D11_BIND_DEPTH_STENCIL;
+	tempDepthBufferDescription.CPUAccessFlags = 0;
+	tempDepthBufferDescription.MiscFlags = 0;
 
-	//TODO^^ binda targetview och depth stencil till output render pipeine
+	// Create the texture for the depth buffer using the filled out description.
 
-	//TODO^^ setup viewport
+	ID3D11Texture2D* tempDepthStencilBuffer = nullptr;
+	tempResult = myDevice->CreateTexture2D(&tempDepthBufferDescription, NULL, &tempDepthStencilBuffer);
+
+	DL_ASSERT(tempResult == S_OK, "Failed to create depth buffer texture");
+
+	// Initialize the description of the stencil state.
+	D3D11_DEPTH_STENCIL_DESC tempDepthStencilDescription;
+	ZeroMemory(&tempDepthStencilDescription, sizeof(tempDepthStencilDescription));
+
+	// Set up the description of the stencil state.
+	tempDepthStencilDescription.DepthEnable = true;
+	tempDepthStencilDescription.DepthWriteMask = D3D11_DEPTH_WRITE_MASK_ALL;
+	tempDepthStencilDescription.DepthFunc = D3D11_COMPARISON_LESS;
+
+	tempDepthStencilDescription.StencilEnable = true;
+	tempDepthStencilDescription.StencilReadMask = 0xFF;
+	tempDepthStencilDescription.StencilWriteMask = 0xFF;
+
+	// Stencil operations if pixel is front-facing.
+	tempDepthStencilDescription.FrontFace.StencilFailOp = D3D11_STENCIL_OP_KEEP;
+	tempDepthStencilDescription.FrontFace.StencilDepthFailOp = D3D11_STENCIL_OP_INCR;
+	tempDepthStencilDescription.FrontFace.StencilPassOp = D3D11_STENCIL_OP_KEEP;
+	tempDepthStencilDescription.FrontFace.StencilFunc = D3D11_COMPARISON_ALWAYS;
+
+	// Stencil operations if pixel is back-facing.
+	tempDepthStencilDescription.BackFace.StencilFailOp = D3D11_STENCIL_OP_KEEP;
+	tempDepthStencilDescription.BackFace.StencilDepthFailOp = D3D11_STENCIL_OP_DECR;
+	tempDepthStencilDescription.BackFace.StencilPassOp = D3D11_STENCIL_OP_KEEP;
+	tempDepthStencilDescription.BackFace.StencilFunc = D3D11_COMPARISON_ALWAYS;
+
+	// Create the depth stencil state.
+	ID3D11DepthStencilState * tempDepthStencilState = nullptr;
+	tempResult = myDevice->CreateDepthStencilState(&tempDepthStencilDescription, &tempDepthStencilState);
+
+	DL_ASSERT(tempResult == S_OK, "Failed to create depth stencil state!");
+
+	// Set the depth stencil state.
+	myDeviceContext->OMSetDepthStencilState(tempDepthStencilState, 1);
+
+	// Initialize the depth stencil view.
+	D3D11_DEPTH_STENCIL_VIEW_DESC tempDepthStencilViewDescription;
+	ZeroMemory(&tempDepthStencilViewDescription, sizeof(tempDepthStencilViewDescription));
+
+	// Set up the depth stencil view description.
+	tempDepthStencilViewDescription.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+	tempDepthStencilViewDescription.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2D;
+	tempDepthStencilViewDescription.Texture2D.MipSlice = 0;
+
+	// Create the depth stencil view.
+	ID3D11DepthStencilView * tempDepthStencilView = nullptr;
+	tempResult = myDevice->CreateDepthStencilView(tempDepthStencilBuffer, &tempDepthStencilViewDescription, &tempDepthStencilView);
+	DL_ASSERT(tempResult == S_OK, "Failed to create depth stencil view!");
+
+	// Bind the render target view and depth stencil buffer to the output render pipeline.
+	myDeviceContext->OMSetRenderTargets(1, &myRenderTargetView, tempDepthStencilView);
+
+	// Setup the raster description which will determine how and what polygons will be drawn.
+	D3D11_RASTERIZER_DESC tempRasterDescription;
+	tempRasterDescription.AntialiasedLineEnable = false;
+	tempRasterDescription.CullMode = D3D11_CULL_BACK;
+	tempRasterDescription.DepthBias = 0;
+	tempRasterDescription.DepthBiasClamp = 0.0f;
+	tempRasterDescription.DepthClipEnable = true;
+	tempRasterDescription.FillMode = D3D11_FILL_SOLID;
+	tempRasterDescription.FrontCounterClockwise = false;
+	tempRasterDescription.MultisampleEnable = false;
+	tempRasterDescription.ScissorEnable = false;
+	tempRasterDescription.SlopeScaledDepthBias = 0.0f;
+
+	// Create the rasterizer state from the description we just filled out.
+	ID3D11RasterizerState *  tempRasterState = nullptr;
+	tempResult = myDevice->CreateRasterizerState(&tempRasterDescription, &tempRasterState);
+	DL_ASSERT(tempResult == S_OK, "Failed to create rasterizer state!");
+
+	// Now set the rasterizer state.
+	myDeviceContext->RSSetState(tempRasterState);
+
+	// Setup the viewport for rendering.
+	D3D11_VIEWPORT tempViewport;
+	tempViewport.Width = static_cast<float>(aScreenWidthHeight.x);
+	tempViewport.Height = static_cast<float>(aScreenWidthHeight.y);
+	tempViewport.MinDepth = 0.0f;
+	tempViewport.MaxDepth = 1.0f;
+	tempViewport.TopLeftX = 0.0f;
+	tempViewport.TopLeftY = 0.0f;
+
+	// Create the viewport.
+	myDeviceContext->RSSetViewports(1, &tempViewport);
+
 }
