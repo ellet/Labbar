2aa6456749d83c3c826d5c80ae6bbf43fd73e4bf
l1 <simon.skogsrydh.sp15@thegameassembly.com>
2016-09-17 13:06:27 +0200

    did work on cbuffers.

--- HUGEnginePrime/Source/Engine/Model/HUGCBuffer.cpp
+++ HUGEnginePrime/Source/Engine/Model/HUGCBuffer.cpp
@@ -1,23 +1,46 @@
 #include "stdafx.h"
 #include "HUGCBuffer.h"
+#include "Engine\HUGEngine.h"
+#include "HUGFramework\HUGDXFramework.h"
 
 
 CHUGCBuffer::CHUGCBuffer()
 {
+	myCurrentIndex = 0;
+	myDataSize = 0;
+	myDataPtr = nullptr;
+	myMatrixBuffer = nullptr;
 }
 
 
 CHUGCBuffer::~CHUGCBuffer()
 {
+	if (myDataPtr != nullptr)
+	{
+		SAFE_DELETE(myDataPtr);
+	}
+
+	if (myMatrixBuffer != nullptr)
+	{
+		myMatrixBuffer->Release();
+		myMatrixBuffer = nullptr;
+	}
 }
 
-void CHUGCBuffer::Init()
+void CHUGCBuffer::Init(const int aSizeOfBuffer)
 {
+	myDataPtr = new char[aSizeOfBuffer];
+	myDataSize = aSizeOfBuffer;
+
+	HRESULT tempResult = S_OK;
+
 	D3D11_BUFFER_DESC tempMatrixBufferDesc;
+
+	ID3D11Device & tempDeviceRef = CHUGEngineSingleton::GetFramework().GetDevice();
 
 	// Setup the description of the dynamic matrix constant buffer that is in the vertex shader.
 	tempMatrixBufferDesc.Usage = D3D11_USAGE_DYNAMIC;
-	tempMatrixBufferDesc.ByteWidth = sizeof(MatrixBuffer);
+	tempMatrixBufferDesc.ByteWidth = sizeof(aSizeOfBuffer);
 	tempMatrixBufferDesc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
 	tempMatrixBufferDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
 	tempMatrixBufferDesc.MiscFlags = 0;
@@ -25,4 +48,9 @@ void CHUGCBuffer::Init()
 
 	// Create the constant buffer pointer so we can access the vertex shader constant buffer from within this class.
 	tempResult = tempDeviceRef.CreateBuffer(&tempMatrixBufferDesc, NULL, &myMatrixBuffer);
+}
+
+void CHUGCBuffer::Activate()
+{
+
 }
--- HUGEnginePrime/Source/Engine/Model/HUGCBuffer.h
+++ HUGEnginePrime/Source/Engine/Model/HUGCBuffer.h
@@ -1,16 +1,29 @@
 #pragma once
+#include <d3d11.h>
 class CHUGCBuffer
 {
 public:
 	CHUGCBuffer();
 	~CHUGCBuffer();
 
-	void Init();
+	void Init(const int aSizeOfBuffer);
 
 	void Activate();
 
-	void SetDAta()
+	template <typename DataType>
+	void SetData(const DataType& aDataToSet);
 
 private:
 	ID3D11Buffer * myMatrixBuffer;
+	char * myDataPtr;
+
+	int myDataSize;
+	int myCurrentIndex;
 };
+
+template <typename DataType>
+void CHUGCBuffer::SetData(const DataType & aDataToSet)
+{
+	&myDataPtr[myCurrentIndex] = &aDataToSet;
+	myCurrentIndex += sizeof(DataType);
+}
