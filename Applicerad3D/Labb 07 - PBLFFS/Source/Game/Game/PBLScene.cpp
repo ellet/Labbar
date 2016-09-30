844e54476a0f39a7e57ea7f12d20887e66987eca
l1 <killallelves@gmail.com>
2016-09-30 12:18:10 +0200

    finished labb and made mockup.

--- Applicerad3D/Labb 07 - PBLFFS/Source/Game/Game/PBLScene.cpp
+++ Applicerad3D/Labb 07 - PBLFFS/Source/Game/Game/PBLScene.cpp
@@ -9,8 +9,9 @@
 #include <Engine\Model\AssimpModel.h>
 #include <Engine/Effect/StandardEffect.h>
 #include <Engine\Camera/Camera.h>
+#include "..\ImGui\imgui.h"
 
-PBLScene::PBLScene()
+PBLScene::PBLScene() : Scene("skybox.dds")
 {
 	CreateFactories();
 
@@ -24,6 +25,75 @@ PBLScene::~PBLScene()
 
 void PBLScene::Update(const Time & aDeltaTime)
 {
+	ImGui::SetNextWindowPos({ 16, 16 }, ImGuiSetCond_Once);
+	ImGui::SetNextWindowSize({ 375, 400 }, ImGuiSetCond_Once);
+	ImGui::SetNextWindowCollapsed(true, ImGuiSetCond_Once);
+
+	if (ImGui::Begin("Controls", nullptr, ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoCollapse))
+	{
+		ImGui::Text("WASD, Arrowkeys, QE");
+	}
+
+	ImGui::End();
+
+
+	if (myRollLeft)
+	{
+		myCamera->GetRototation().RotateZ(-aDeltaTime.InSeconds());
+	}
+	if (myRollRight)
+	{
+		myCamera->GetRototation().RotateZ(aDeltaTime.InSeconds());
+	}
+
+	const float speed = 2.f;
+
+	if (myMoveLeft)
+	{
+		myCamera->AddPosition(myCamera->GetRototation().GetLeft() * speed * aDeltaTime.InSeconds());
+	}
+	if (myMoveRight)
+	{
+		myCamera->AddPosition(myCamera->GetRototation().GetRight() * speed * aDeltaTime.InSeconds());
+	}
+
+	if (myMoveForward)
+	{
+		myCamera->AddPosition(myCamera->GetRototation().GetForward() * speed * aDeltaTime.InSeconds());
+	}
+	if (myMoveBackward)
+	{
+		myCamera->AddPosition(myCamera->GetRototation().GetBackward() * speed * aDeltaTime.InSeconds());
+	}
+
+	if (myMoveUp)
+	{
+		myCamera->AddPosition(myCamera->GetRototation().GetUpward() * speed * aDeltaTime.InSeconds());
+	}
+	if (myMoveDown)
+	{
+		myCamera->AddPosition(myCamera->GetRototation().GetDownward() * speed * aDeltaTime.InSeconds());
+	}
+
+	const float rotateSpeed = 1.f;
+
+	if (myPitchForward)
+	{
+		myCamera->GetRototation().RotateX(rotateSpeed * aDeltaTime.InSeconds());
+	}
+	if (myPitchBackward)
+	{
+		myCamera->GetRototation().RotateX(-rotateSpeed * aDeltaTime.InSeconds());
+	}
+	if (myRotateLeft)
+	{
+		myCamera->GetRototation().RotateY(rotateSpeed * aDeltaTime.InSeconds());
+	}
+	if (myRotateRight)
+	{
+		myCamera->GetRototation().RotateY(-rotateSpeed * aDeltaTime.InSeconds());
+	}
+
 	Scene::Update(aDeltaTime);
 }
 
@@ -48,4 +118,102 @@ void PBLScene::CreateFactories()
 	//GetCamera().SetPosition(model->GetBoundingBox().GetCenter() + Vector3f(0.f, 0.f, -model->GetBoundingBox().GetSize().z * 1.5f));
 
 	enemy->AddComponent<ModelComponent>(prettyModel);
+}
+
+ReceiveResult PBLScene::Receive(const AnyKeyDownMessage& aMessage)
+{
+	switch (aMessage.key)
+	{
+	case KeyboardKey::eQ:
+		myRollLeft = true;
+		break;
+	case KeyboardKey::eE:
+		myRollRight = true;
+		break;
+
+	case KeyboardKey::eA:
+		myMoveLeft = true;
+		break;
+	case KeyboardKey::eD:
+		myMoveRight = true;
+		break;
+	case KeyboardKey::eW:
+		myMoveForward = true;
+		break;
+	case KeyboardKey::eS:
+		myMoveBackward = true;
+		break;
+
+	case KeyboardKey::eLeft:
+		myRotateLeft = true;
+		break;
+	case KeyboardKey::eRight:
+		myRotateRight = true;
+		break;
+	case KeyboardKey::eUp:
+		myPitchForward = true;
+		break;
+	case KeyboardKey::eDown:
+		myPitchBackward = true;
+		break;
+
+	case KeyboardKey::eControl:
+		myMoveDown = true;
+		break;
+	case KeyboardKey::eSpace:
+		myMoveUp = true;
+		break;
+
+	case KeyboardKey::eAlt:
+		Engine::GetLogger().LogInfo("eAlt pressed");
+		break;
+	}
+	return ReceiveResult::eContinue;
+}
+
+ReceiveResult PBLScene::Receive(const AnyKeyUpMessage& aMessage)
+{
+	switch (aMessage.key)
+	{
+	case KeyboardKey::eQ:
+		myRollLeft = false;
+		break;
+	case KeyboardKey::eE:
+		myRollRight = false;
+		break;
+
+	case KeyboardKey::eA:
+		myMoveLeft = false;
+		break;
+	case KeyboardKey::eD:
+		myMoveRight = false;
+		break;
+	case KeyboardKey::eW:
+		myMoveForward = false;
+		break;
+	case KeyboardKey::eS:
+		myMoveBackward = false;
+		break;
+
+	case KeyboardKey::eLeft:
+		myRotateLeft = false;
+		break;
+	case KeyboardKey::eRight:
+		myRotateRight = false;
+		break;
+	case KeyboardKey::eUp:
+		myPitchForward = false;
+		break;
+	case KeyboardKey::eDown:
+		myPitchBackward = false;
+		break;
+
+	case KeyboardKey::eControl:
+		myMoveDown = false;
+		break;
+	case KeyboardKey::eSpace:
+		myMoveUp = false;
+		break;
+	}
+	return ReceiveResult::eContinue;
 }
--- Applicerad3D/Labb 07 - PBLFFS/Source/Game/Game/PBLScene.h
+++ Applicerad3D/Labb 07 - PBLFFS/Source/Game/Game/PBLScene.h
@@ -2,7 +2,7 @@
 
 #include <Engine/Scene/Scene.h>
 
-class PBLScene : public Scene
+class PBLScene : public Scene, public Subscriber<AnyKeyDownMessage>, public Subscriber<AnyKeyUpMessage>
 {
 public:
 	PBLScene();
@@ -13,7 +13,23 @@ public:
 
 	virtual void Render() override;
 
+	ReceiveResult Receive(const AnyKeyDownMessage & aMessage) override;
+	ReceiveResult Receive(const AnyKeyUpMessage & aMessage) override;
+
 private:
 	void CreateFactories();
+	bool myRotateLeft;
+	bool myRotateRight;
+	bool myMoveLeft;
+	bool myMoveRight;
+	bool myMoveForward;
+	bool myMoveBackward;
+
+	bool myPitchForward;
+	bool myPitchBackward;
+	bool myRollLeft;
+	bool myRollRight;
+	bool myMoveDown;
+	bool myMoveUp;
 
 };
