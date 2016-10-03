c216c3a2b0e683317e3316a9dacb810595e3c456
Ylf skola <ylva.werner.sp15@thegameassembly.com>
2016-10-03 14:22:24 +0200

    simd labb

---
+++ Tillämpad MjukvaruUtveckling/Labb 06 - YLF - SIMD/SIMDmatrix/SIMDmatrix/Time/Stopwatch.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "Time/Time.h"
+#include <time.h>
+#include <windows.h>
+
+//class Time;
+//
+//class Stopwatch
+//{
+//public:
+//	Stopwatch();
+//	~Stopwatch();
+//
+//	void Restart();
+//
+//	Time GetElapsedTime() const;
+//
+//private:
+//	LONGLONG myStartTime;
+//	float myFrequency;
+//};
+//
