19223dc8ea8c22a7616ee0d8c2cb0a78e8f8bf55
l1 <simon.skogsrydh.sp15@thegameassembly.com>
2016-10-03 16:05:30 +0200

    fasteru matrixeru.

---
+++ Tillämpad MjukvaruUtveckling/Labb 06 - SIMD/SIMD/Stopwatch.h
@@ -0,0 +1,16 @@
+#pragma once
+
+class Stopwatch
+{
+public:
+	Stopwatch();
+	~Stopwatch();
+
+	void Restart();
+
+	Time GetElapsedTime() const;
+
+private:
+	LONGLONG myStartTime;
+	float myFrequency;
+};
